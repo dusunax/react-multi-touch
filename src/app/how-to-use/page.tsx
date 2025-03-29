aab105cdc76f19c0152d365de11085792c116315
Dusuna <94776135+dusunax@users.noreply.github.com>
2025-03-29 09:14:52 +0900

    refactor: simplify threshold in page

--- src/app/how-to-use/page.tsx
+++ src/app/how-to-use/page.tsx
@@ -1,29 +1,27 @@
 "use client";
 import Image from "next/image";
-import { useRef } from "react";
 import Touchable from "src/packages/components/package/touchable/Touchable";
 
 const Page = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const [maxTrashhold, minTrashhold] = [window.innerWidth - 60, 100];
 
   return (
-    <div className="m-8" ref={containerRef}>
+    <div className="m-8">
       <Touchable
         id="test-1"
-        maxTrashhold={containerRef.current?.clientWidth}
-        minTrashhold={80}
+        maxTrashhold={maxTrashhold}
+        minTrashhold={minTrashhold}
       >
         <div className="bg-blue-500 w-28 h-28">
           Touchable
           <p className="text-xs truncate">
-            max-width is {containerRef.current?.clientWidth}
+            max size is {maxTrashhold}
             <br />
-            min-width is {80}
+            min size is {minTrashhold}
           </p>
         </div>
         <Touchable.Handles />
       </Touchable>
-
       <Touchable id="test-2" className="-mt-16 ml-4">
         <Image
           src="/sun.png"
